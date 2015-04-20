7dfcf2f45b6d2147a8b79f16cb53b5327e2d7d92
Fiaz Sami <fiaz.sami@collecdev.com>
2015-04-20 10:47:00 -0500

    added recognizer for determining who is being quoted

--- Quotables/Utilities.swift
+++ Quotables/Utilities.swift
@@ -31,3 +31,19 @@ func adjustFontSize(label: UILabel, size: CGFloat) {
         let font = label.font.fontWithSize(size)
         label.font = font
 }
+
+func getPersonName(str: String) -> String {
+    let options: NSLinguisticTaggerOptions = .OmitWhitespace | .OmitPunctuation | .JoinNames
+    let schemes = NSLinguisticTagger.availableTagSchemesForLanguage("en")
+    let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
+    var personName = ""
+    tagger.string = str
+    tagger.enumerateTagsInRange(NSMakeRange(0, (str as NSString).length), scheme: NSLinguisticTagSchemeNameTypeOrLexicalClass, options: options) { (tag, tokenRange, sentenceRange, _) in
+        let token = (str as NSString).substringWithRange(tokenRange)
+        if tag == "PersonalName" {
+            personName = token
+        }
+    }
+
+    return personName
+}
