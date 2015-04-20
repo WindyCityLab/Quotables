434239a105e1cb8a44c5b3b6ae9257583571dbeb
Fiaz Sami <fiaz.sami@collecdev.com>
2015-04-20 11:58:54 -0500

    added SHA1 for determining content uniqueness

--- Quotables/Quotables-Bridging-Header.h
+++ Quotables/Quotables-Bridging-Header.h
@@ -6,9 +6,5 @@
 //  Copyright (c) 2015 windycitylabs. All rights reserved.
 //
 
-#ifndef Quotables_Quotables_Bridging_Header_h
-#define Quotables_Quotables_Bridging_Header_h
-
 #import <CommonCrypto/CommonCrypto.h>
 
-#endif
--- Quotables/Utilities.swift
+++ Quotables/Utilities.swift
@@ -58,3 +58,14 @@ func getPersonName(str: String) -> String {
 
     return personName
 }
+
+func getSHA1(input: String) -> String {
+    let data = input.dataUsingEncoding(NSUTF8StringEncoding)!
+    var digest = [UInt8](count:Int(CC_SHA1_DIGEST_LENGTH), repeatedValue: 0)
+    CC_SHA1(data.bytes, CC_LONG(data.length), &digest)
+    let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
+    for byte in digest {
+        output.appendFormat("%02x", byte)
+    }
+    return output as String
+}
