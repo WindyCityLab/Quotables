9d6dbcb63afdd341b0567f27394c6f32f131ce3c
Fiaz Sami <fiaz.sami@collecdev.com>
2015-04-20 11:50:08 -0500

    added bridging-header

---
+++ Quotables/Quotables-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Quotables-Bridging-Header.h
+//  Quotables
+//
+//  Created by Fiaz Sami on 4/20/15.
+//  Copyright (c) 2015 windycitylabs. All rights reserved.
+//
+
+#ifndef Quotables_Quotables_Bridging_Header_h
+#define Quotables_Quotables_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif
